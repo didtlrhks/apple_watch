3aed777b2fe25ad2effaf14999a023dbe3635294
sikwan <yysskk99999@naver.com>
2025-03-31 17:58:34 +0900

    color add

--- bno_apple_watch Watch App/ContentView.swift
+++ bno_apple_watch Watch App/ContentView.swift
@@ -8,12 +8,13 @@ struct Food: Identifiable {
 
 struct ContentView: View {
     @State private var foods = [
-        Food(name: "김치찌개", imageName: "fork.knife"),
-        Food(name: "라면", imageName: "leaf"),
-        Food(name: "비빔밥", imageName: "cup.and.saucer"),
-        Food(name: "돈까스", imageName: "star"),
-        Food(name: "국수", imageName: "leaf"),
-        Food(name: "부대찌개", imageName: "heart")
+        Food(name: "김치찌개", imageName: "bulgogi"),
+        Food(name: "간장게장", imageName: "squid"),
+        Food(name: "비빔밥", imageName: "bulgogi"),
+        Food(name: "간장게장", imageName: "squid"),
+        Food(name: "국수", imageName: "bulgogi"),
+        Food(name: "간장게장", imageName: "squid"),
+        Food(name: "부대찌개", imageName: "bulgogi")
     ]
     
     @State private var selectedFood: Food?
@@ -22,44 +23,50 @@ struct ContentView: View {
     @State private var timer: Timer?
     
     var body: some View {
-        VStack(spacing: 10) {
-            Text("오늘의 점심은?")
-                .font(.title3)
-                .bold()
+        ZStack {
+            //FDE8BB
+            Color(hex: "FDE8BB")
+                .edgesIgnoringSafeArea(.all)
             
-            ZStack {
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(Color.gray.opacity(0.1))
-                    .frame(width: 80, height: 80)
+            VStack(spacing: 10) {
+                Text("오늘의 점심은?")
+                    .font(.custom("NotoSansOriya-Bold", size: 16))
+                    .foregroundColor(.black)
                 
-                Image(systemName: foods[currentIndex].imageName)
-                    .imageScale(.large)
-                    .font(.system(size: 30))
+                ZStack {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color.gray.opacity(0.0))
+                        .frame(width: 80, height: 80)
+                    
+                    Image(foods[currentIndex].imageName)
+                        .imageScale(.large)
+                        .font(.custom("NotoSansOriya-Bold", size: 16))
+                }
+                
+                if let selectedFood = selectedFood {
+                    Text(selectedFood.name)
+                        .font(.headline)
+                        .bold()
+                } else {
+                    Text("메뉴를 선택해주세요")
+                        .font(.custom("NotoSansOriya-Bold", size: 16))
+                        .foregroundColor(.gray)
+                }
+                
+                Button(action: {
+                    startSpinning()
+                }) {
+                    Text("메뉴 선택하기")
+                        .font(.custom("NotoSansOriya-Bold", size: 16))
+                        .foregroundColor(.white)
+                        .frame(width: 180, height: 50)
+                        .background(Color(hex: "FF6B35"))
+                        .cornerRadius(15)
+                }
+                .allowsHitTesting(!isSpinning)
             }
-            
-            if let selectedFood = selectedFood {
-                Text(selectedFood.name)
-                    .font(.headline)
-                    .bold()
-            } else {
-                Text("메뉴를 선택해주세요")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-            }
-            
-            Button(action: {
-                startSpinning()
-            }) {
-                Text("메뉴 선택하기")
-                    .font(.footnote)
-                    .foregroundColor(.white)
-                    .frame(width: 180, height: 50)
-                    .background(Color.blue)
-                    .cornerRadius(15)
-            }
-            .allowsHitTesting(!isSpinning)
+            .padding(8)
         }
-        .padding(8)
     }
     
     private func startSpinning() {
@@ -80,6 +87,33 @@ struct ContentView: View {
             
             isSpinning = false
         }
+    }
+}
+
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
 
---
+++ bno_apple_watch Watch App/Font_Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Font_Extension.swift
+//  bno_apple_watch Watch App
+//
+//  Created by 양시관 on 3/31/25.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Font{
+    static let ongleip32 : Font = .custom("온글잎 박다현체", size:32)
+    static let ongleip16 : Font = .custom("온글잎 박다현체", size:16)
+}
+
